7f0a76ccfae4914fe193df2b9f212f5a1f78a357
aurnabbarua02 <aurnabbarua02@gmail.com>
2024-09-04 00:12:02 +0600

    modified

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+    document.getElementById("light-mode-icon").classList.toggle("active");
+    document.getElementById("dark-mode-icon").classList.toggle("active");
+}
